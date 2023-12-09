c62e97f9a6858fd1046ba46ad1b014418c1cd22d
SA-OBAiDELLAH <152771194+SA-OBAiDELLAH@users.noreply.github.com>
2023-12-09 13:29:49 +0800

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -45,6 +45,28 @@ s1, s2 = 100, 150
 
 e1, e2 = s1+ZMR_ZML() , s2+ZMR_ZML()
  
+path = r'C:/Users/saoba/OneDrive/Desktop/New folder/image1.jpg'
+image = cv2.imread(path) 
+image1 = Image.open(path)
+print (image1.width)
+print (image1.height)
+
+# Creating a black screen image using numpy.zeros function 
+Img = np.zeros((400, 400, 3), dtype='uint8') 
+# Start coordinate, here (100, 100). It represents the top left corner of image 
+start_point = (100, 100) 
+# End coordinate, here (450, 450). It represents the bottom right corner of the image according to resolution 
+end_point = (400, 280) 
+# White color in BGR 
+color = (255, 0, 0) 
+# Line thickness of 9 px 
+thickness = 5
+# Using cv2.line() method to draw a diagonal green line with thickness of 9 px 
+image1 = cv2.line(image, start_point, end_point, color, thickness) 
+# Display the image 
+#cv2.imshow('Drawing_Line', image) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
 
 img = pil.Image.open('image.jpg')
 img1 = pil.Image.open('image1.jpg')
@@ -52,5 +74,5 @@ img1 = pil.Image.open('image1.jpg')
 #path = r'C:/Users/saoba/OneDrive/Desktop/New folder/image1.jpg'
 #image = cv2.imread(path) 
 
-col2.image(img, caption='CT Scan for Skull')
-
+#col2.image(img, caption='CT Scan for Skull')
+col2.image(image1, caption='CT Scan for Skull')
