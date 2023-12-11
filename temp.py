766654115c27a5e3b2022fdd6c442b0313cf5469
SA-OBAiDELLAH <sa.obaidellah@Gmail.com>
2023-12-11 18:56:03 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -8,48 +8,83 @@ st.write ("""# Syndromic Craniosynostosis""")
 #st.sidebar.header('input')
 col1, col2= st.columns([1,3])
 
+def fNBa(X,Y):
+    y=-1.554+1.021*X+.753*Y
+    return y
+
+def ZMR_ZML(Z):
+    y=5.762+0.920*Z   
+    return y
+
 col1.subheader('Input')
 #col1.write('Right Canal')
-SN=col1.number_input("SN",value=float(0))
-SBa=col1.number_input("SBa",value=float(0))
+SN=col1.number_input("SN",value=None)
+SBa=col1.number_input("SBa",value=None)
 
+if SN==None and SBa==None :
+    NBa=col1.number_input("NBa",value=None)
 
-#col1.write('Right Canal')
-#H_OS_L=col1.number_input("H.OS.L",value=float())
-#Perimeter_L=col1.number_input("Perimeter.L",value=float())
-#Q3=col1.number_input("Q3",value=float())
+ZML_ZMR=col1.number_input("ZML-ZMR",value=None)
 
-def NBa():
-    y=-1.554+1.021*SN+.753*SBa
-   
-    return y
-
-def ZMR_ZML():
-    y=5.762+0.920*NBa()
+if SN==None and SBa==None and NBa==None or ZML_ZMR==None :
+ col2.markdown( "### :red[Please Enter the Patient's Measurments]")
+elif SN!=None and SBa!=None and ZML_ZMR!=None :
     
-    return y
+    col1.write("NBa")
+    col1.write(fNBa(SN,SBa))
 
-col2.subheader(' The probability of Diagnosis',)
-col2.write("###### ZMR-ZML")
-col2.write(ZMR_ZML())
+    col2.subheader(' The Diagnosis',)
+    col2.write("###### Expected ZMR-ZML")
+    col2.write(ZMR_ZML(fNBa(SN,SBa)))
+    col2.write("The Volume of Movment is")
+    col2.write(ZMR_ZML(fNBa(SN,SBa))-ZML_ZMR)
 
-s1, s2 = 100, 150
+    s1, s2 = 100, 150
+    e1, e2 = s1+ZMR_ZML(fNBa(SN,SBa)) , s2+ZMR_ZML(fNBa(SN,SBa))
+    
+    path = r'C:/Users/saoba/OneDrive/Desktop/New folder/image1.jpg'
+    myimage = cv2.imread(path) 
+    image1 = cv2.imread(path)
 
-e1, e2 = s1+ZMR_ZML() , s2+ZMR_ZML()
- 
+    start_point = (100, 100) 
+    # End coordinate, here (450, 450). It represents the bottom right corner of the image according to resolution 
+    end_point = (400, 280) 
+    # White color in BGR 
+    color = (255, 0, 0) 
+    # Line thickness of 9 px 
+    thickness = 5
+    # Using cv2.line() method to draw a diagonal green line with thickness of 9 px 
+    myimage = cv2.line(myimage, start_point, end_point, color, thickness)
 
-path = r'C:/Users/saoba/OneDrive/Desktop/New folder/image1.jpg'
-myimage = cv2.imread(path) 
+    if ZMR_ZML(fNBa(SN,SBa)) < 4.35 :
+        col2.image(image1, caption='CT Scan for Skull')
+    elif ZMR_ZML(fNBa(SN,SBa)) > 4.35 :
+        col2.image(myimage, caption='CT Scan for Skull')
+elif ZML_ZMR!=None and NBa!=None :
+    col2.subheader(' The Diagnosis',)
+    col2.write("###### Expected ZMR-ZML")
+    col2.write(ZMR_ZML(NBa))
+    col2.write("The Volume of Movment is")
+    col2.write(ZMR_ZML(NBa)-ZML_ZMR)
 
-start_point = (100, 100) 
-# End coordinate, here (450, 450). It represents the bottom right corner of the image according to resolution 
-end_point = (400, 280) 
-# White color in BGR 
-color = (255, 0, 0) 
-# Line thickness of 9 px 
-thickness = 5
-# Using cv2.line() method to draw a diagonal green line with thickness of 9 px 
-myimage = cv2.line(myimage, start_point, end_point, color, thickness)
+    s1, s2 = 100, 150
+    e1, e2 = s1+ZMR_ZML(NBa) , s2+ZMR_ZML(NBa)
+    
+    path = r'C:/Users/saoba/OneDrive/Desktop/New folder/image1.jpg'
+    myimage = cv2.imread(path) 
+    image1 = cv2.imread(path)
 
-#col2.image(img, caption='CT Scan for Skull')
-col2.image(myimage, caption='CT Scan for Skull')
+    start_point = (100, 100) 
+    # End coordinate, here (450, 450). It represents the bottom right corner of the image according to resolution 
+    end_point = (400, 280) 
+    # White color in BGR 
+    color = (255, 0, 0) 
+    # Line thickness of 9 px 
+    thickness = 5
+    # Using cv2.line() method to draw a diagonal green line with thickness of 9 px 
+    myimage = cv2.line(myimage, start_point, end_point, color, thickness)
+
+    if ZMR_ZML(NBa) < 4.35 :
+        col2.image(image1, caption='CT Scan for Skull')
+    elif ZMR_ZML(NBa) > 4.35 :
+        col2.image(myimage, caption='CT Scan for Skull')
